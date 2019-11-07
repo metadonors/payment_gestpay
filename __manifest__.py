e9efdceaa965c17584fb7cfd028b9ac199591f71
Fabrizio Arzeni <fabrizio.arzeni@metadonors.it>
2019-11-07 12:52:44 +0100

    Initial

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2019 Metadonors Srl
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Payment Gestpay',
+    'summary': """
+        Payment Acquirer: Gestpay Implementation""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Metadonors Srl,Odoo Community Association (OCA)',
+    'website': 'https://www.metadonors.it',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
